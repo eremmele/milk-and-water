aeaf0780a88ddc7f26a8bb343502156c814e6d26
eremmele <ericaremmele@gd-36ef2a.mica.edu>
2016-03-11 12:34:43 -0500

    update

--- grid.js
+++ grid.js
@@ -1,13 +1,8 @@
+var bgcolorlist=new Array("silver", "#BAF3C3", "#c3baf3")
 
-//
+$(".color").css("background-color",bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]);
 
-$(".jump-response").each(function() {
-    var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
-    $(this).css("background-color", hue);
-});
-
-
-//////////
+///////////////////////////////////////////////
 
 var colors = ["#1abc9c","#2ecc71","#3498db","#9b59b6","#34495e","#f1c40f","#e67e22","#e74c3c","#ecf0f1"],
     colorsDark  = ["#16a085","#27ae60","#2980b9","#8e44ad","#2c3e50","#f39c12","#d35400","#c0392b","#bdc3c7"];
